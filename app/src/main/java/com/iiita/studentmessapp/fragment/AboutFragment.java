09d2448733ef25ad7ba01755acdeb816593aa04c
Pranjal <pranjalg2308@gmail.com>
2018-09-26 23:55:29 +0530

    image from glide

--- app/src/main/java/com/iiita/studentmessapp/fragment/AboutFragment.java
+++ app/src/main/java/com/iiita/studentmessapp/fragment/AboutFragment.java
@@ -10,11 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.iiita.studentmessapp.MainActivity;
 import com.iiita.studentmessapp.R;
 import com.iiita.studentmessapp.UserSignIn.Login;
@@ -28,12 +35,14 @@ public class AboutFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser currentFirebaseUser;
     private View view;
     private static Button signOutButton;
     private String mParam1;
     private String mParam2;
-
-
+    private TextView usernameTv;
+    private TextView usermailId;
+    private ImageView userDisplayPicture;
     public AboutFragment() {
         // Required empty public constructor
     }
@@ -50,7 +59,7 @@ public class AboutFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -75,7 +84,30 @@ public class AboutFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_about, container, false);
         setSignOutFunction();
+        setupUserProfile();
         return view;
+    }
+
+    private void setupUserProfile() {
+        currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        usernameTv = view.findViewById(R.id.user_name);
+        usermailId = view.findViewById(R.id.user_mail_id);
+        userDisplayPicture = view.findViewById(R.id.profile_image);
+        usernameTv.setText(currentFirebaseUser.getDisplayName());
+        usermailId.setText(currentFirebaseUser.getEmail());
+        Uri userPhotoUrl  = currentFirebaseUser.getPhotoUrl();
+        String originalPieceOfUrl = "s96-c/photo.jpg";
+        String newUrl="";
+        String newPieceOfUrlToAdd = "s400-c/photo.jpg";
+        if (userPhotoUrl != null) {
+            String photoPath = userPhotoUrl.toString();
+             newUrl = photoPath.replace(originalPieceOfUrl, newPieceOfUrlToAdd);
+        }
+        RequestOptions options = new RequestOptions();
+        options.circleCrop();
+        options.format(DecodeFormat.PREFER_ARGB_8888);
+        options.override(Target.SIZE_ORIGINAL);
+        Glide.with(getContext()).load(newUrl).apply(options).into(userDisplayPicture);
     }
 
     private void setSignOutFunction() {
